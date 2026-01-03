9fa591d0cf2319f0a66cab1c3244dae93e7500ee
Carlos Bolívar <cebemind@hotmail.com>
2026-01-03 15:18:05 -0400

    feat: Initialize project with Tailwind CSS and shadcn/ui for styling and component utilities.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './globals.css'
 import App from './App.tsx'
 import './config/i18n'
 
